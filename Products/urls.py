762e4df634a2fa2e67e937512ac11519bb0de076
Vladislav Ainshtein <vladislav.ainshtein@gmail.com>
2022-05-05 03:43:50 -0700

    fucking cart eeee

--- Products/urls.py
+++ Products/urls.py
@@ -1,7 +1,10 @@
+from re import A
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('cart/', views.cart),
+    path('add_product/<int:id>/', views.add_product)
 ]
--- Products/views.py
+++ Products/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Product
 
 # Create your views here.
 def index(request):
     all_products = Product.objects.all()
     return render(request, 'main.html', {"products": all_products})
+
+def cart(request):
+    cart = request.session['cart']
+    cart = [Product.objects.get(pk=id) for id in cart]
+    print(cart)
+    return render(request, 'cart.html', {'cart': cart})
+
+def add_product(request, id):
+    session = request.session
+    try:
+        session['cart'].append(id)
+    except:
+        session['cart'] = [id]
+    return HttpResponse('<script type="text/javascript">window.close()</script>')  
